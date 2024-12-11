8b5bb2d9fad0b744ad8a29e28f8cacebd541295a
Thomas Li <tli186@ucr.edu>
2024-12-11 00:51:48 -0700

    implement multithreaded search

--- WORKSPACE
+++ WORKSPACE
@@ -1,3 +1,13 @@
 workspace(
     name = "tetrsolver",
 )
+
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
+git_repository(
+    name = "parlaylib",
+    build_file = "@//externals:BUILD.parlaylib.bazel",
+    commit = "36459f42a84207330eae706c47e6fab712e6a149",
+    recursive_init_submodules = True,
+    remote = "git@github.com:cmuparlay/parlaylib.git",
+)
--- src/search_1p.cc
+++ src/search_1p.cc
@@ -2,6 +2,8 @@
  * game tree search for 1 player
  */
 
+#include <parlay/parallel.h>
+
 #include <algorithm>
 #include <iomanip>
 #include <tuple>
@@ -18,7 +20,7 @@ int32_t main() {
   pb.set_current_piece(&pieces::T);
   std::cout << pb << "\n";
 
-  for (int i = 0; i < 10000; ++i) {
+  for (int i = 0; i < 20; ++i) {
     int best_placement_id = -1;
     double best_eval = -1e9;
     auto placements = pb.GeneratePlacements();
@@ -38,24 +40,71 @@ int32_t main() {
     const int max_depth = 6;
     for (int curr_level = 2; curr_level <= max_depth; ++curr_level) {
       std::sort(curr_id.rbegin(), curr_id.rend());
-      for (size_t j = 0; j < std::min<size_t>(1000, curr_id.size()); ++j) {
-        ++nodes_explored;
+
+      // sequential search
+      // for (size_t j = 0; j < std::min<size_t>(1000, curr_id.size()); ++j) {
+      //   ++nodes_explored;
+      //   auto [eval, id, pb_idx] = curr_id[j];
+      //   auto& pb = curr_pb[pb_idx];
+      //   if (curr_level == max_depth) {
+      //     if (eval > best_eval) {
+      //       best_eval = eval;
+      //       best_placement_id = id;
+      //     }
+      //   }
+      //   for (auto [x, y, d] : pb.GeneratePlacements()) {
+      //     PlayerBoard pb_(pb);
+      //     pb_.SimulatePlacement(x, y, d);
+      //     double eval = pb_.Evaluate();
+      //     next_id.push_back({eval, id, next_id.size()});
+      //     next_pb.push_back(pb_);
+      //   }
+      // };
+
+      // multithreaded search
+      std::vector<std::vector<std::tuple<double, int, int>>> next_ids(1000);
+      std::vector<std::vector<PlayerBoard>> next_pbs(1000);
+
+      auto ExploreNodeIndex = [&](size_t j) -> void {
         auto [eval, id, pb_idx] = curr_id[j];
         auto& pb = curr_pb[pb_idx];
-        if (curr_level == max_depth) {
-          if (eval > best_eval) {
-            best_eval = eval;
-            best_placement_id = id;
-          }
-        }
-        for (auto [x, y, d] : pb.GeneratePlacements()) {
+        auto placements = pb.GeneratePlacements();
+        for (size_t k = 0; k < std::min<size_t>(100, placements.size()); ++k) {
+          auto [x, y, d] = placements[k];
           PlayerBoard pb_(pb);
           pb_.SimulatePlacement(x, y, d);
           double eval = pb_.Evaluate();
+          next_ids[j].push_back({eval, id, 0});
+          next_pbs[j].push_back(pb_);
+        }
+      };
+      const size_t max_j = std::min<size_t>(1000, curr_id.size());
+      // multithreaded mode (of parallel code)
+      parlay::parallel_for(0, std::min<size_t>(1000, curr_id.size()),
+                           ExploreNodeIndex);
+
+      // sequential mode (of parallel code)
+      // for (size_t j = 0; j < max_j; ++j) {
+      //   ExploreNodeIndex(j);
+      // }
+
+      // collecting results
+      for (size_t j = 0; j < max_j; ++j) {
+        for (size_t k = 0; k < next_ids[j].size(); ++k) {
+          auto [eval, id, pb_idx] = next_ids[j][k];
+          auto pb_ = next_pbs[j][k];
           next_id.push_back({eval, id, next_id.size()});
-          next_pb.push_back(pb_);
+          next_pb.push_back(std::move(pb_));
+
+          if (curr_level == max_depth) {
+            if (eval > best_eval) {
+              best_eval = eval;
+              best_placement_id = id;
+            }
+          }
         }
       }
+
       curr_pb.clear();
       curr_id.clear();
       std::swap(curr_pb, next_pb);
